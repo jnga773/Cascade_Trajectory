484a54246e317acff4998550620037f7231fef64
vcanela <victorscc@gmail.com>
2018-11-08 10:38:27 +1300

    Added benchmark test
    The Fortran file, cc.f90, will produce an output, BMtest.txt, which will be compared to what should be the correct output, BMoriginal.txt. This comparison is done by running the python script BM.py. In short, to do a test of updates made on cc.f90:
    1. Compile cc.f90
    2. Run compiled cc.f90
    3. Run BM.py

---
+++ BMtest/BM.py
@@ -0,0 +1,36 @@
+from pylab import *
+
+fo = open('./BMoriginal.txt','r')
+ft = open('./BMtest.txt','r')
+lo = fo.readlines()
+lt = ft.readlines()
+fo.close()
+ft.close()
+
+column_tag = ['timestep',
+              'last section',
+              'photon number a',
+              'photon number b',
+              'probability of state g',
+              'probability of state e',
+              'probability of state f',
+              'probability of atom decay',
+              'probability of transmission a',
+              'probability of reflection a',
+              'probability of transmission b',
+              'probability of reflection b']
+
+for j in range(len(lo)):
+    tempo = [float(k) for k in lo[j].split()]
+    tempt = [float(k) for k in lt[j].split()]
+    for l in range(2,12):
+        if (tempo[l] != tempt[l]):
+            print('*********************')            
+            print('Deviation detected!')
+            print('*********************')
+            print('Time step = ' + str(tempt[0]))
+            print('Last section visited = ' + str(tempt[1]))
+            print('Affected value = ' + column_tag[l])
+            print('Percentage difference = '
+                  + str(100.0*(tempo[l] - tempt[l])/tempo[l]) + '%')
+            print(' ')
