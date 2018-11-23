6f2ab5dd39ee1c1fa6648c5b83e2f53f00b04ee4
Chris Scott <chris@chrisdjscott.co.uk>
2018-11-23 13:33:07 +1300

    Bm.py takes reference file as argument

--- BMtest/BM.py
+++ BMtest/BM.py
@@ -1,5 +1,15 @@
 
-fo = open('./BMoriginal.txt','r')
+from __future__ import print_function
+import os
+import sys
+
+# different compilers give different results so
+# read in the reference file from the command line
+if len(sys.argv) != 2 or not os.path.exists(sys.argv[1]):
+    print("Usage: python BM.py <REFERENCE_FILE>")
+    sys.exit(1)
+
+fo = open(sys.argv[1],'r')
 ft = open('./BMtest.txt','r')
 lo = fo.readlines()
 lt = ft.readlines()
@@ -19,6 +29,7 @@ column_tag = ['timestep',
               'probability of transmission b',
               'probability of reflection b']
 
+count_diff = 0
 for j in range(len(lo)):
     tempo = [float(k) for k in lo[j].split()]
     tempt = [float(k) for k in lt[j].split()]
@@ -33,3 +44,7 @@ for j in range(len(lo)):
             print('Percentage difference = '
                   + str(100.0*(tempo[l] - tempt[l])/tempo[l]) + '%')
             print(' ')
+            count_diff += 1
+
+if count_diff > 0:
+    sys.exit(1)
